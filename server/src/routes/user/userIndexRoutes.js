d91f3832a2196423a8dd85da25c53711be1f7d76
snehatiwari972 <snehatiwari972@gmail.com>
2025-10-22 15:51:11 +0530

    create and integrate dashboard api

--- server/src/routes/user/userIndexRoutes.js
+++ server/src/routes/user/userIndexRoutes.js
@@ -6,6 +6,7 @@ import userWishlistRoutes from './userWishlistRoutes.js'
 // import userOrderRoutes from './userOrderRoutes.js'
 import userSupportRoutes from './userSupportRoutes.js'
 import userRoutes from './userRoutes.js'
+import userDashboardRoutes from "./userDashboardRoutes.js"
 
 const router = express.Router()
 
@@ -16,5 +17,6 @@ router.use("/wishlist", userWishlistRoutes)
 // router.use("/order", userOrderRoutes)
 router.use("/support", userSupportRoutes)
 router.use("/profile", userRoutes)
+router.use("/dashboard", userDashboardRoutes)
 
 export default router
